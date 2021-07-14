10dfd0ed62001f9805bfd225c49a67098f1522c1
nooken <ernis@deveem.io>
2021-07-14 16:01:23 +0600

    Home: Add UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_api_with_retrofit/presentation/home/home.dart';
 
 void main() {
@@ -8,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.blue));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
           title: 'Movie Popular',
--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -5,7 +5,6 @@ import 'package:flutter_api_with_retrofit/domain/model/popular_entity.dart';
 import 'package:flutter_api_with_retrofit/presentation/bloc/home_bloc.dart';
 import 'package:flutter_api_with_retrofit/presentation/movie_info/movie_info.dart';
 import 'package:flutter_api_with_retrofit/utils/constants.dart';
-import 'package:flutter_api_with_retrofit/utils/utils.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -17,7 +16,8 @@ class Home extends StatelessWidget {
       child: Consumer<HomeBloc>(builder: (context, _homeBloc, child) {
         return SafeArea(
           child: Scaffold(
-            backgroundColor: Color.fromARGB(100, 25, 45, 54),
+            appBar: AppBar(title: Text('Popular Movie')),
+            backgroundColor: Colors.grey,
             body: StreamBuilder<PopularEntity>(
               stream: _homeBloc.subjectMoviePopular.stream,
               builder: (context, AsyncSnapshot<PopularEntity> snapshot) {
@@ -58,21 +58,19 @@ class Home extends StatelessWidget {
 
   Widget _buildGridView(BuildContext context, PopularEntity movie) {
     return GridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: 1,
         children: List.generate(movie.results.length, (index) {
-          return Align(
-            heightFactor: 200.0,
-            alignment: Alignment.center,
-            child: Card(
-              margin: EdgeInsets.all(16),
-              child: InkResponse(
-                onTap: () {
-                  _onTapHolder(context, movie.results[index]);
-                },
+          return Container(
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
+            color: Colors.white,
+            child: InkResponse(
+              onTap: () {
+                _onTapHolder(context, movie.results[index]);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(children: [
-                  _itemImage(movie.results[index]),
-                  _itemTitle(movie.results[index]),
-                  _itemDescription(movie.results[index])
+                  _item(movie.results[index]),
                 ]),
               ),
             ),
@@ -85,38 +83,48 @@ class Home extends StatelessWidget {
         MaterialPageRoute(builder: (context) => MovieInfo(popularResults.id)));
   }
 
-  _itemImage(PopularResults popularResults) {
-    return Expanded(
-        child: Card(
-            margin: EdgeInsets.zero,
-            child: Image.network("$MOVIE_BASE_URL${popularResults.posterPath}",
-                fit: BoxFit.fitWidth, width: double.infinity)));
-  }
-
-  _itemTitle(PopularResults popularResults) {
-    return Align(
-      alignment: Alignment.bottomLeft,
-      child: Container(
-        margin: EdgeInsets.fromLTRB(12, 8, 0, 0),
-        child: Text(popularResults.title,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis),
-      ),
-    );
-  }
-
-  _itemDescription(PopularResults result) {
-    return Align(
-      alignment: Alignment.bottomLeft,
-      child: Container(
-        margin: EdgeInsets.fromLTRB(12, 4, 0, 4),
-        child: Text(Utils.parseDateToddMMyyyy(result.releaseDate),
-            style: const TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 12, color: Colors.grey),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis),
-      ),
+  _item(PopularResults popularResults) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      verticalDirection: VerticalDirection.down,
+      children: [
+        Column(
+          children: [
+            Image.network("$MOVIE_BASE_URL${popularResults.posterPath}",
+                fit: BoxFit.fitWidth, width: 160),
+            Text(popularResults.releaseDate,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center),
+            Row(
+              children: [
+                Text(popularResults.voteCount.toString(),
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                Padding(padding: EdgeInsets.fromLTRB(32, 0, 32, 0)),
+                Text(popularResults.voteAverage.toString(),
+                    style: const TextStyle(fontWeight: FontWeight.bold))
+              ],
+            )
+          ],
+        ),
+        Flexible(
+            child: Container(
+                padding: EdgeInsets.fromLTRB(16, 0, 0, 8),
+                child: ListTile(
+                  dense: true,
+                  contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
+                  title: Text(popularResults.title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis),
+                  subtitle: Text(popularResults.overview,
+                      maxLines: 15,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                          color: Colors.grey)),
+                ))),
+      ],
     );
   }
 }
