d2df8c21e93bc65c47ad416ba88e3210606c16c3
nooken <ernis@deveem.io>
2021-07-14 18:51:29 +0600

    MovieInfo: Add UI

--- lib/presentation/movie_info/movie_info.dart
+++ lib/presentation/movie_info/movie_info.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_api_with_retrofit/domain/model/movie_info_entity.dart';
 import 'package:flutter_api_with_retrofit/presentation/bloc/movie_info_bloc.dart';
 import 'package:flutter_api_with_retrofit/utils/constants.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:provider/provider.dart';
 
 class MovieInfo extends StatelessWidget {
@@ -19,7 +20,6 @@ class MovieInfo extends StatelessWidget {
       child: Consumer<MovieInfoBloc>(builder: (context, _movieInfoBloc, child) {
         return SafeArea(
           child: Scaffold(
-            backgroundColor: Color.fromARGB(100, 25, 45, 54),
             body: StreamBuilder<MovieInfoEntity>(
               stream: _movieInfoBloc.subjectMovieInfo.stream,
               builder: (context, AsyncSnapshot<MovieInfoEntity> snapshot) {
@@ -57,24 +57,85 @@ class MovieInfo extends StatelessWidget {
     ));
   }
 
-  Widget _buildMovieInfo(BuildContext context, MovieInfoEntity movie) {
-    return Container(
-        alignment: Alignment.topLeft,
-        child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                  width: 200,
-                  height: 300,
-                  child: Image.network(MOVIE_BASE_URL + movie.posterPath),
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.fromLTRB(16, 32, 16, 16)),
-              Expanded(
-                  child: Container(
-                      margin: EdgeInsets.fromLTRB(8, 48, 0, 0),
-                      child: Text(movie.title,
-                          style: TextStyle(fontSize: 24, color: Colors.cyan)))),
-            ]));
+  Widget _buildMovieInfo(
+      BuildContext context, MovieInfoEntity movieInfoEntity) {
+    return Scaffold(
+      appBar: AppBar(title: Text(movieInfoEntity.title)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Center(
+              child: Image.network(
+                  "$MOVIE_BASE_URL${movieInfoEntity.posterPath}",
+                  fit: BoxFit.fitWidth),
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: Text(movieInfoEntity.title,
+                      overflow: TextOverflow.ellipsis,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                ),
+                RatingBar(
+                  initialRating: movieInfoEntity.voteAverage / 2,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  ratingWidget: RatingWidget(
+                      full: Image.asset('images/ic_star_full.png'),
+                      half: Image.asset('images/ic_star_half.png'),
+                      empty: Image.asset('images/ic_star_empty.png')),
+                  itemSize: 25,
+                  minRating: 1,
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+              ],
+            ),
+            Text(movieInfoEntity.releaseDate,
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            Text(movieInfoEntity.overview,
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            Text("Genres",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text(_getGenres(movieInfoEntity.genres),
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            Text("Run time",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Text("${movieInfoEntity.runtime.toString()} min",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  _getGenres(List<MovieInfoGenres> genres) {
+    String allGenres = "";
+
+    for (var i = 0; i < genres.length; i++) {
+      if (i != genres.length - 1) {
+        allGenres += "${genres[i].name} / ";
+      } else {
+        allGenres += genres[i].name;
+      }
+    }
+    return allGenres;
   }
 }
